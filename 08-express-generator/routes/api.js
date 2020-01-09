573b00c46f05a5de53a3f5d4e17b2d212a9ed1cf
pepesan <pepesan@gmail.com>
2020-01-09 17:01:04 +0100

    mejora del api rest

--- 08-express-generator/routes/api.js
+++ 08-express-generator/routes/api.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var api=require("../controllers/apiController");
-/* GET home page. */
+/* Estandar by Pepesan */
 router.get('/', api.index);
 router.post('/', api.add);
-router.post('/edit/:id', api.edit);
 router.get('/:id', api.show);
+router.post('/edit/:id', api.edit);
+router.put('/:id', api.edit);
 router.get('/delete/:id', api.deleteBook);
+router.delete('/:id', api.deleteBook);
+
+/*
+ # Standart de facto
+ # Get / Listado
+router.get('/', api.index);
+ # POST / Añadir (datos)
+router.post('/', api.add);
+ # GET /:id Obtener un objeto por ID
+router.get('/:id', api.show);
+ # PUT /:id Modificar un objeto por ID
+router.put('/:id', api.edit);
+ # Delete /:id Borrar un objeto por ID
+router.delete('/:id', api.deleteBook);
+ */
 
 module.exports = router;
