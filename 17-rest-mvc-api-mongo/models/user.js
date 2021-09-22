7d2aca04ad4fe31d6c67a916b5b8e6edec13724b
David Vaquero <pepesan@gmail.com>
2021-09-22 17:24:48 +0200

    quitado warning de mongoose

--- 17-rest-mvc-api-mongo/models/user.js
+++ 17-rest-mvc-api-mongo/models/user.js
@@ -2,14 +2,13 @@ var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 var crypto = require('crypto');
 
-
 var UserSchema = new mongoose.Schema({
     username: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
     bio: String,
     image: String,
     hash: String,
     salt: String,
-    
+
 }, {timestamps: true,collection:"user"});
 
 UserSchema.methods.setPassword = function(password){
